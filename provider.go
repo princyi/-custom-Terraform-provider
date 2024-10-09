96461d725cbbcce80dbccd28ca3de9f650d88115
Princy Kshirsagar <112242191+princyi@users.noreply.github.com>
2024-10-09 11:15:02 +0530

    Create provider.go

---
+++ provider.go
@@ -0,0 +1,13 @@
+package example
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"example_resource": resourceExample(),
+		},
+	}
+}
